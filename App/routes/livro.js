743eecc79992d928bf7132b2fa987ffdb94c8a53
Mario Henrique Pereira da Rosa <mariopereira398@gmail.com>
2020-01-25 15:06:45 -0300

    Cadastro de um novo livro, ajustes no layout

--- App/routes/livro.js
+++ App/routes/livro.js
@@ -5,12 +5,33 @@ var consMysql = 'mysql://root:@localhost:3306/Desafio';
 
 router.post('/', function (req, res) {
     let livro = [];
-    const connection = mysql.createConnection(consMysql);
-
     livro.push(req.body.situacao);
     livro.push(req.body.idLivro);
 
     var sql = "UPDATE LIVRO SET situacao = ? WHERE numero = ?";
+
+    const connection = mysql.createConnection(consMysql);
+    connection.connect();
+    connection.query(sql, livro, function (error, result) {
+        if (error) {
+            console.log(error)
+            return res.status(304).end();
+        }
+        return res.status(200).end();
+    });
+    connection.end();
+});
+router.post('/add', function (req, res) {
+    let livro = [];
+    livro.push(req.body.titulo);
+    livro.push(req.body.autor);
+    livro.push(req.body.ano);
+    livro.push(req.body.situacao);
+
+    var sql = "INSERT INTO LIVRO (titulo, autor, ano, situacao) VALUES (?,?,?,?);";
+    
+    const connection = mysql.createConnection(consMysql);
+    connection.connect();
     connection.query(sql, livro, function (error, result) {
         if (error) {
             console.log(error)
@@ -25,7 +46,8 @@ router.delete('/:id', function (req, res, next) {
     let usuario = [];
     usuario.push(req.params.id);
     const connection = mysql.createConnection(consMysql);
-    let sql = "DELETE FROM Exames WHERE id = ?";
+    let sql = "DELETE FROM LIVRO WHERE numero = ?";
+    connection.connect();
     connection.query(sql, usuario, function (error, results) {
         if (error) {
             return res.status(304).end();
@@ -36,20 +58,23 @@ router.delete('/:id', function (req, res, next) {
     connection.end();
 });
 
-router.put('/:id', function (req, res) {// fazer ainda tirar duvida com Prof. Carlos
-    let cliente = [];
-    cliente.push(req.body.nome);
-    cliente.push(req.body.email);
-    cliente.push(req.body.telefone);
-    cliente.push(req.params.id);
+router.put('/:id', function (req, res) {
+    let livro = [];
+    livro.push(req.body.titulo);
+    livro.push(req.body.autor);
+    livro.push(req.body.ano);
+    livro.push(req.body.situacao);
+    livro.push(req.body.numero);
+
+    let sql = "UPDATE LIVRO SET titulo=? ,autor=? ,ano=?, situacao=? WHERE numero = ?;";
 
     const connection = mysql.createConnection(consMysql);
-    connection.query("UPDATE TABELA SET nome=? ,email=? ,telefone=? WHERE id = ?", cliente, async function (error, results) {
+    connection.connect();
+    connection.query(sql, livro, async function (error, results) {
         if (error) {
             return res.status(304).end();
         }
-        let resposta = results[0];
-        return res.status(200).send(resposta);
+        return res.status(200).end();
     });
     connection.end();
 });
--- appWeb/routes/livro.js
+++ appWeb/routes/livro.js
@@ -7,39 +7,19 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-
-    var novaSituacao;
-    var aux;
-    if (req.body.situacao == "Disponivel") {
-        novaSituacao = "Indisponivel";
-        aux = -1;
-    }
-    else {
-        novaSituacao = "Disponivel";
-        aux = 1;
-    }
-
-    axios.post('http://localhost:3000/emprestimo',
+    console.log(req.body.autor);
+    console.log(req.body.ano);
+    console.log(req.body.titulo);
+    axios.post('http://localhost:3000/livro/add',
         {
-            idUsuario: 1,
-            idLivro: req.body.idLivro,
-            situacao: aux,
-            idEmprestimo: req.body.idEmprestimo
+            titulo   : req.body.titulo,
+            autor    : req.body.autor,
+            ano      : req.body.ano,
+            situacao : "Disponivel"
         }
     ).then(function (response) {
         if (response.status == 200) {
-            axios.post('http://localhost:3000/livro',
-                {
-                    idLivro: req.body.idLivro,
-                    situacao: novaSituacao
-                }
-            ).then(function (response) {
-                if (response.status == 200) {
-                    res.redirect('biblioteca');
-                }
-            }).catch(error => {
-                console.log(error)
-            });
+            res.redirect('biblioteca');
         }
     }).catch(error => {
         console.log(error)
